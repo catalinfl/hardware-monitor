52d84b2ec8dd0ec22d531e318bc796fc3cd2012d
catalinfl <catalinfl02@gmail.com>
2024-09-15 00:54:16 +0300

    rename

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 	"strconv"
@@ -39,7 +38,6 @@ func main() {
 
 		for {
 			_, message, err := conn.ReadMessage()
-			fmt.Println(string(message))
 			if err != nil {
 				fmt.Println(err)
 				break
@@ -238,98 +236,98 @@ func main() {
 		}
 	})
 
-	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+	// http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
 
-		if r.Method == "GET" {
-			fmt.Println("Request received")
-			memInfo, err := mem.VirtualMemory()
+	// 	if r.Method == "GET" {
+	// 		fmt.Println("Request received")
+	// 		memInfo, err := mem.VirtualMemory()
 
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
+	// 		if err != nil {
+	// 			fmt.Println(err)
+	// 			return
+	// 		}
 
-			hostInfo, err := host.Info()
+	// 		hostInfo, err := host.Info()
 
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
+	// 		if err != nil {
+	// 			fmt.Println(err)
+	// 			return
+	// 		}
 
-			memoryJSON := map[string]string{
-				"Total":       fmt.Sprintf("%.3f GB", float64(memInfo.Total)/float64(1<<30)),
-				"Available":   fmt.Sprintf("%.3f GB", float64(memInfo.Available)/float64(1<<30)),
-				"Used":        fmt.Sprintf("%.3f GB", float64(memInfo.Used)/float64(1<<30)),
-				"Free":        fmt.Sprintf("%.3f GB", float64(memInfo.Free)/float64(1<<30)),
-				"UsedPercent": fmt.Sprintf("%d%%", int(memInfo.UsedPercent)),
-			}
+	// 		memoryJSON := map[string]string{
+	// 			"Total":       fmt.Sprintf("%.3f GB", float64(memInfo.Total)/float64(1<<30)),
+	// 			"Available":   fmt.Sprintf("%.3f GB", float64(memInfo.Available)/float64(1<<30)),
+	// 			"Used":        fmt.Sprintf("%.3f GB", float64(memInfo.Used)/float64(1<<30)),
+	// 			"Free":        fmt.Sprintf("%.3f GB", float64(memInfo.Free)/float64(1<<30)),
+	// 			"UsedPercent": fmt.Sprintf("%d%%", int(memInfo.UsedPercent)),
+	// 		}
 
-			bootTime := int64(hostInfo.BootTime)
-			uptime := int64(hostInfo.Uptime)
+	// 		bootTime := int64(hostInfo.BootTime)
+	// 		uptime := int64(hostInfo.Uptime)
 
-			days := uptime / (24 * 3600)
-			hours := (uptime % (24 * 3600)) / 3600
-			minutes := (uptime % 3600) / 60
-			seconds := uptime % 60
+	// 		days := uptime / (24 * 3600)
+	// 		hours := (uptime % (24 * 3600)) / 3600
+	// 		minutes := (uptime % 3600) / 60
+	// 		seconds := uptime % 60
 
-			uptimeString := fmt.Sprintf("%d days, %d hours, %d minutes %d seconds", days, hours, minutes, seconds)
-			formattedBootTime := time.Unix(bootTime, 0).Format("2006-01-02 15:04:05")
+	// 		uptimeString := fmt.Sprintf("%d days, %d hours, %d minutes %d seconds", days, hours, minutes, seconds)
+	// 		formattedBootTime := time.Unix(bootTime, 0).Format("2006-01-02 15:04:05")
 
-			hostJSON := map[string]string{
-				"Host":            hostInfo.Hostname,
-				"OS":              strings.ToUpper(hostInfo.OS[:1]) + hostInfo.OS[1:],
-				"Platform":        hostInfo.Platform,
-				"PlatformFamily":  hostInfo.PlatformFamily,
-				"PlatformVersion": hostInfo.PlatformVersion,
-				"KernelVersion":   hostInfo.KernelVersion,
-				"KernelArch":      hostInfo.KernelArch,
-				"BootTime":        formattedBootTime,
-				"Procs":           fmt.Sprintf("%d", hostInfo.Procs),
-				"Uptime":          uptimeString,
-			}
+	// 		hostJSON := map[string]string{
+	// 			"Host":            hostInfo.Hostname,
+	// 			"OS":              strings.ToUpper(hostInfo.OS[:1]) + hostInfo.OS[1:],
+	// 			"Platform":        hostInfo.Platform,
+	// 			"PlatformFamily":  hostInfo.PlatformFamily,
+	// 			"PlatformVersion": hostInfo.PlatformVersion,
+	// 			"KernelVersion":   hostInfo.KernelVersion,
+	// 			"KernelArch":      hostInfo.KernelArch,
+	// 			"BootTime":        formattedBootTime,
+	// 			"Procs":           fmt.Sprintf("%d", hostInfo.Procs),
+	// 			"Uptime":          uptimeString,
+	// 		}
 
-			processes, err := process.Processes()
+	// 		processes, err := process.Processes()
 
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
+	// 		if err != nil {
+	// 			fmt.Println(err)
+	// 			return
+	// 		}
 
-			processJSON := make([]map[string]string, 0)
+	// 		processJSON := make([]map[string]string, 0)
 
-			for _, p := range processes {
-				name, _ := p.Name()
-				pid := p.Pid
-				ppid, _ := p.Ppid()
-				username, _ := p.Username()
-				cmdline, _ := p.Cmdline()
+	// 		for _, p := range processes {
+	// 			name, _ := p.Name()
+	// 			pid := p.Pid
+	// 			ppid, _ := p.Ppid()
+	// 			username, _ := p.Username()
+	// 			cmdline, _ := p.Cmdline()
 
-				// if len(cmdline) > 100 {
-				// 	cmdline = cmdline[:100]
-				// }
+	// 			// if len(cmdline) > 100 {
+	// 			// 	cmdline = cmdline[:100]
+	// 			// }
 
-				processJSON = append(processJSON, map[string]string{
-					"Name":     name,
-					"PID":      fmt.Sprintf("%d", pid),
-					"PPID":     fmt.Sprintf("%d", ppid),
-					"Username": username,
-					"Cmdline":  cmdline,
-				})
-			}
+	// 			processJSON = append(processJSON, map[string]string{
+	// 				"Name":     name,
+	// 				"PID":      fmt.Sprintf("%d", pid),
+	// 				"PPID":     fmt.Sprintf("%d", ppid),
+	// 				"Username": username,
+	// 				"Cmdline":  cmdline,
+	// 			})
+	// 		}
 
-			infoJSON := map[string]interface{}{
-				"memory":    memoryJSON,
-				"host":      hostJSON,
-				"processes": processJSON,
-			}
+	// 		infoJSON := map[string]interface{}{
+	// 			"memory":    memoryJSON,
+	// 			"host":      hostJSON,
+	// 			"processes": processJSON,
+	// 		}
 
-			w.Header().Set("Content-Type", "application/json")
+	// 		w.Header().Set("Content-Type", "application/json")
 
-			json.NewEncoder(w).Encode(infoJSON)
-		}
-	})
+	// 		json.NewEncoder(w).Encode(infoJSON)
+	// 	}
+	// })
 
-	component := hello("world")
+	component := mainComp()
 	http.Handle("/", templ.Handler(component))
 
 	http.ListenAndServe(":8080", nil)
